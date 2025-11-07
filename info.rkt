1bd8efbdbd16a647851f8e5045305a4c8c34797d
PlexiglasOG <vakoum2018@gmail.com>
2025-11-07 17:46:03 +0100

    Initial Commit

---
+++ info.rkt
@@ -0,0 +1,13 @@
+#lang info
+(define framework:color-schemes
+  '(#hash((colors
+           .
+           ((framework:syntax-color:scheme:string
+             #(226 0 116))
+            (framework:syntax-color:scheme:constant
+             #(226 0 116))
+            (framework:syntax-color:scheme:comment
+             #(226 0 116))
+            (framework:syntax-color:scheme:parenthesis
+             #(226 0 116))))
+          (name . "TeleRacket"))))
