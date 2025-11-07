3dff776ddf101835d327659f76024ae6458f3544
PlexiglasOG <vakoum2018@gmail.com>
2025-11-07 17:58:38 +0100

    Updated some colors

--- info.rkt
+++ info.rkt
@@ -8,6 +8,19 @@
              #(226 0 116))
             (framework:syntax-color:scheme:comment
              #(226 0 116))
+             (framework:syntax-color:scheme:error
+             #(226 0 116))
+             (framework:syntax-color:scheme:hash-colon-keyword
+             #(226 0 116))
+             (framework:syntax-color:scheme:keyword
+             #(226 0 116))
+             (framework:syntax-color:scheme:other
+             #(226 0 116))
+             (framework:syntax-color:scheme:symbol
+             #(226 0 116))
+             (framework:syntax-color:scheme:text
+             #(226 0 116))
             (framework:syntax-color:scheme:parenthesis
              #(226 0 116))))
           (name . "TeleRacket"))))
+
